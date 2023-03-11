46eb1064befd7d456ff0ae9dc81dbc3c13a0bde9
Mike Jackson <perigran_falcon@yahoo.com>
2023-03-11 06:28:31 -0700

    Add files via upload

---
+++ register.c
@@ -0,0 +1,16 @@
+// register.c
+
+#include <stdio.h>
+
+int main(void)
+{
+    register int num = 0;
+
+    while (num < 5)
+    {
+        ++num;
+        printf("Pass %d\n", num);
+    }
+
+    return 0;
+}
