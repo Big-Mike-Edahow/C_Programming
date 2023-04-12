ffd40e3bbac18f0f4f5c9a7f66067a16a90c7aa3
Mike Jackson <perigran_falcon@yahoo.com>
2023-04-12 12:38:43 -0600

    Add files via upload

---
+++ conditional.c
@@ -0,0 +1,20 @@
+// conditional.c
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 8;
+
+    (num % 2 != 0) ?
+    printf("%d is odd\n", num) : printf("%d is even\n", num);
+
+    printf("There %s", (num == 1) ? "is " : "are ");
+    printf("%d %s\n", num, (num == 1) ? "apple" : "apples");
+
+    num = 2;
+    printf("There %s", (num == 1) ? "is " : "are ");
+    printf("%d %s\n", num, (num == 1) ? "apple" : "apples");
+
+    return 0;
+}
