b6ec1ccd402a0cc57005ed910af639eff152f7b5
Mike Jackson <perigran_falcon@yahoo.com>
2023-04-19 12:00:51 -0600

    Add files via upload

---
+++ square.c
@@ -0,0 +1,31 @@
+// square.c
+
+#include <stdio.h>
+#include "utils.h"
+
+void getnum();
+
+int main()
+{
+    getnum();
+
+    return 0;
+}
+
+void getnum() {
+    int num;
+    char again;
+
+    printf("Enter an integer to be squared: ");
+    scanf("%d", &num);
+
+    printf("%d squared is %d\n", num, square(num));
+
+    printf("Square another number? y or n: ");
+    scanf("%1s", &again);
+
+    if((again == 'Y') || (again == 'y')) {
+        getnum();
+    }
+    else return;
+}
