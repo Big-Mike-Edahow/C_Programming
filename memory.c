0cf73dd0fb9a1b8bca343de94b6aadcf8f49d576
Mike Jackson <perigran_falcon@yahoo.com>
2023-06-11 09:46:06 -0600

    Add files via upload

---
+++ memory.c
@@ -0,0 +1,39 @@
+// memory.c
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int malloc_usable_size(int *bytes);
+
+int main()
+{
+    int size;
+    int *mem;
+
+    mem = malloc(100 * sizeof(int));
+
+    if (mem != NULL) {
+        size = malloc_usable_size(mem);
+        printf("\nSize of block for 100 ints: %d bytes\n", size);
+        printf("Beginning at %p\n", mem);
+    }
+    else {
+        printf("!!! Insufficient memory\n");
+        return 1;
+    }
+
+    mem = realloc(mem, size + (100 * sizeof(int)));
+
+    if (mem != NULL) {
+        size = malloc_usable_size(mem);
+        printf("\nSize of bloc for 200 ints: %d bytes\n", size);
+        printf("Beginning at %p\n", mem);
+    }
+    else {
+        printf("!!! Insufficient memory\n");
+        return 1;
+    }
+
+    free(mem);
+    return 0;
+}
