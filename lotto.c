5388a84fb7bc659560905ce8caf81e73740f85c8
Mike Jackson <perigran_falcon@yahoo.com>
2023-06-12 13:08:23 -0600

    Add files via upload

---
+++ lotto.c
@@ -0,0 +1,38 @@
+// lotto.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main()
+{
+    int i;
+    int r;
+    int temp;
+    int nums[60];
+    char buf[4];
+    char str[100] = {"Your six luck numbers are: "};
+
+    srand(time(NULL));
+    for (i = 0; i < 60; i++) {
+        nums[i]= i;
+    }
+
+    for (i = 1; i < 60; i++) {
+        r = (rand() % 59) + 1;
+        temp = nums[i];
+        nums[i] = nums[r];
+        nums[r] = temp;
+    }
+
+    for (i = 1; i < 7; i++) {
+        sprintf(buf, "%d ", nums[i]);
+        strcat(buf, " ");
+        strcat(str, buf);
+    }
+
+    printf("\n%s\n\n", str);
+
+    return 0;
+}
